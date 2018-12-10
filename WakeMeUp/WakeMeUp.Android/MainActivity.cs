69508ff6027453be4f83f458669545141d0e1f78
Karm <karmjit435@gmail.com>
2018-12-10 16:42:30 +1000

    Added google maps to the app.

--- WakeMeUp/WakeMeUp.Android/MainActivity.cs
+++ WakeMeUp/WakeMeUp.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Plugin.Permissions;
 
 namespace WakeMeUp.Droid
 {
@@ -19,7 +20,16 @@ namespace WakeMeUp.Droid
 
             base.OnCreate(savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            Xamarin.FormsMaps.Init(this, savedInstanceState);
+
             LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
